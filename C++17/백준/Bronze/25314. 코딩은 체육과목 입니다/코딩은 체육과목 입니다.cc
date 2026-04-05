306ac13913edcff9a2d60127b7ffb4a40c635c8f
jimin414 <jiminmyung05@gmail.com>
2026-04-05 17:00:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int count = N / 4;  
+
+    for (int i = 0; i < count; i++) {
+        cout << "long ";
+    }
+    cout << "int";
+}
